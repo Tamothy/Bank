12e977236f145ee5d85f4096a3ba46c6c95024b3
April Peng and Tam Nguyen <helloaprilpeng@gmail.com>
2016-07-06 19:37:58 -0700

    append deposit/withdraw scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 //business logic
-function Customer(name, deposit) {
+function BankAccount(name, deposit) {
   this.customerName = name;
   this.initialDeposit = deposit;
+}
+
+BankAccount.prototype.depositWithdraw = function(deposit, withdraw) {
+  return this.initialDeposit + deposit - withdraw;
 }
 
 //user interface logic
@@ -12,8 +16,22 @@ $(document).ready(function () {
     var inputtedName = $("input#new-customer-name").val();
     var inputtedInitialDeposit = $("input#new-customer-initial-deposit").val();
 
-    var newCustomer = new Customer(inputtedName, inputtedInitialDeposit);
+    var newCustomer = new BankAccount(inputtedName, inputtedInitialDeposit);
 
     $("ul#customers").append("<li><span class='customer'>" + newCustomer.customerName + newCustomer.initialDeposit + "</span></li>");
+
+    $("ul#customers").append('<div class="deposit">' +
+                             '<div class="form-group">' +
+                               '<label for="deposit">Deposit</label>' +
+                               '<input type="text" class="form-control deposit">' +
+                             '</div>' +
+                             '<div class="form-group">' +
+                               '<label for="withdraw">Withdraw</label>' +
+                               '<input type="text" class="form-control withdraw">' +
+                             '</div>' +
+                           '</div>');
+
+    $("ul#customers").append('<button type="submit" class="btn">Submit</button>');
+
   });
 });
